ea67f0a741c4f1920ecc915b0f4247466bed64a1
fowr <89118232+perdedora@users.noreply.github.com>
2024-09-27 20:20:17 -0300

    use css to hide instead of getting all elements and apply inline

--- js/fix-report-delete-submit.js
+++ js/fix-report-delete-submit.js
@@ -1,23 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    hideInput(document);
-    hidePostModerations();
-});
-
-function hideInput(el) {
-    const inputs = el.querySelectorAll('input.delete')
-    inputs.forEach(input => {
-        input.style.display = 'none';
+    Vichan.createElement('style', {
+        text: `
+            input.delete, #post-moderation-fields {
+                display: none;
+            }
+        `,
+        parent: document.head
     });
-}
-
-function hidePostModerations() {
-    const fields = document.querySelector('#post-moderation-fields');
-    if (fields) {
-        fields.remove();
-    }
-}
-
-document.addEventListener('new_post_js', (post) => {
-    const newPost = post.detail.detail;
-    hideInput(newPost);
 });
