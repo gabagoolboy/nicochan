72babeb99854dd28d92929e6ab7ea225b91e1c06
fowr <89118232+perdedora@users.noreply.github.com>
2024-10-05 11:58:07 -0300

    format antibot and use mt_rand instead of random_int

--- inc/anti-bot.php
+++ inc/anti-bot.php
@@ -6,29 +6,30 @@
 
 defined('TINYBOARD') or exit;
 
-$hidden_inputs_twig = array();
-
-class AntiBot {
+class AntiBot
+{
     public string $salt;
     public array $inputs = [];
     private int $index = 0;
     private array $config;
 
-    public function __construct(array $config, array $salt = []) {
+    public function __construct(array $config, array $salt = [])
+    {
         $this->config = $config;
         $this->salt = !empty($salt) ? implode(':', $salt) : '';
 
         shuffle($this->config['spam']['hidden_input_names']);
-        $inputCount = random_int($this->config['spam']['hidden_inputs_min'], $this->config['spam']['hidden_inputs_max']);
+        $inputCount = mt_rand($this->config['spam']['hidden_inputs_min'], $this->config['spam']['hidden_inputs_max']);
         $this->generateInputs($inputCount);
     }
 
-    private function generateInputs(int $inputCount): void {
+    private function generateInputs(int $inputCount): void
+    {
         $hiddenInputIndex = 0;
 
         for ($i = 0; $i < $inputCount; $i++) {
-            $name = ($hiddenInputIndex === false || random_int(0, 2) === 0)
-                ? self::randomString(random_int(10, 40), false, false, $this->config['spam']['unicode'])
+            $name = ($hiddenInputIndex === false || mt_rand(0, 2) === 0)
+                ? self::randomString(mt_rand(10, 40), false, false, $this->config['spam']['unicode'])
                 : $this->config['spam']['hidden_input_names'][$hiddenInputIndex++];
 
             if ($hiddenInputIndex >= count($this->config['spam']['hidden_input_names'])) {
@@ -39,15 +40,17 @@ class AntiBot {
         }
     }
 
-    private function generateInputValue(): string {
-        return match (random_int(0, 4)) {
+    private function generateInputValue(): string
+    {
+        return match (mt_rand(0, 4)) {
             0, 1 => '',
-            2, 3 => (string)random_int(0, 100000),
-            default => self::randomString(random_int(5, 100), true, true, $this->config['spam']['unicode'])
+            2, 3 => (string)mt_rand(0, 100000),
+            default => self::randomString(mt_rand(5, 100), true, true, $this->config['spam']['unicode'])
         };
     }
 
-    public static function randomString(int $length, bool $uppercase = false, bool $specialChars = false, bool $unicodeChars = false): string {
+    public static function randomString(int $length, bool $uppercase = false, bool $specialChars = false, bool $unicodeChars = false): string
+    {
         $chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
         $chars .= $uppercase ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : '';
         $chars .= $specialChars ? ' ~!@#$%^&*()_+,./;\'[]\\{}|:<>?=-` ' : '';
@@ -62,16 +65,18 @@ class AntiBot {
         return implode('', array_slice($charsArray, 0, $length));
     }
 
-    private static function generateUnicodeChars(): string {
+    private static function generateUnicodeChars(): string
+    {
         $unicodeChars = '';
         $len = strlen($unicodeChars) / 10;
         for ($n = 0; $n < $len; $n++) {
-            $unicodeChars .= mb_convert_encoding('&#' . random_int(0x2600, 0x26FF) . ';', 'UTF-8', 'HTML-ENTITIES');
+            $unicodeChars .= mb_convert_encoding('&#' . mt_rand(0x2600, 0x26FF) . ';', 'UTF-8', 'HTML-ENTITIES');
         }
         return $unicodeChars;
     }
 
-    public function html(?int $count = null): string {
+    public function html(?int $count = null): string
+    {
         $elements = [
             '<input type="hidden" name="%name%" value="%value%">',
             '<input type="hidden" value="%value%" name="%name%">',
@@ -81,7 +86,7 @@ class AntiBot {
             '<textarea style="display:none" name="%name%">%value%</textarea>'
         ];
 
-        $count = $count ?? random_int(1, (int) abs(count($this->inputs) / 15) + 1);
+        $count = $count ?? mt_rand(1, (int) abs(count($this->inputs) / 15) + 1);
         $inputs = array_slice($this->inputs, $this->index, $count === true ? null : $count);
         $this->index += count($inputs);
 
@@ -94,11 +99,12 @@ class AntiBot {
         return $html;
     }
 
-    private function getRandomElement(array $elements): string {
+    private function getRandomElement(array $elements): string
+    {
         $element = null;
         while (!$element) {
             $element = str_replace(' ', self::space(), $elements[array_rand($elements)]);
-            if (random_int(0, 5) === 0) {
+            if (mt_rand(0, 5) === 0) {
                 $element = str_replace('>', self::space() . '>', $element);
             }
             if (strpos($element, 'textarea') !== false && trim($element) === '') {
@@ -108,24 +114,27 @@ class AntiBot {
         return $element;
     }
 
-    private function renderElement(string $element, string $name, string $value): string {
+    private function renderElement(string $element, string $name, string $value): string
+    {
         $element = str_replace('%name%', utf8tohtml($name), $element);
-        $value = random_int(0, 2) === 0 ? self::makeConfusing($value) : utf8tohtml($value);
+        $value = mt_rand(0, 2) === 0 ? self::makeConfusing($value) : utf8tohtml($value);
         if (strpos($element, 'textarea') === false) {
             $value = str_replace('"', '&quot;', $value);
         }
         return str_replace('%value%', $value, $element);
     }
 
-    public static function space(): string {
-        return random_int(0, 3) !== 0 ? ' ' : str_repeat(' ', random_int(1, 3));
+    public static function space(): string
+    {
+        return mt_rand(0, 3) !== 0 ? ' ' : str_repeat(' ', mt_rand(1, 3));
     }
 
-    public static function makeConfusing(string $string): string {
+    public static function makeConfusing(string $string): string
+    {
         $chars = preg_split('//u', $string, -1, PREG_SPLIT_NO_EMPTY);
 
         foreach ($chars as &$char) {
-            $char = random_int(0, 3) !== 0
+            $char = mt_rand(0, 3) !== 0
                 ? utf8tohtml($char)
                 : mb_encode_numericentity($char, [0, 0xFFFF, 0, 0xFFFF], 'UTF-8');
         }
@@ -133,11 +142,13 @@ class AntiBot {
         return implode('', $chars);
     }
 
-    public function reset(): void {
+    public function reset(): void
+    {
         $this->index = 0;
     }
 
-    public function hash(): string {
+    public function hash(): string
+    {
         ksort($this->inputs);  // Sort inputs alphabetically
 
         $hash = '';
